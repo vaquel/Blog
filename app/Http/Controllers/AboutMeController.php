12a1b49c35d5b962df9395dee6b7ca567b492187
陈龙 <503526664@qq.com>
2017-11-02 14:50:35 +0800

    增加aboutme

---
+++ app/Http/Controllers/AboutMeController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: a123456
+ * Date: 17/11/2
+ * Time: 下午2:24
+ */
+
+namespace App\Http\Controllers;
+
+
+class AboutMeController
+{
+
+}
---
+++ resources/views/me/aboutMe.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: a123456
+ * Date: 17/11/2
+ * Time: 下午2:26
+ */
